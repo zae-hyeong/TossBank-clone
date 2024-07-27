0580ab8da952526244ed8478818d371e83211b4b
zae-hyeong <p0138@naver.com>
2024-07-27 21:43:11 +0900

    [feat] add detail description at product-service

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,4 +12,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextVideo(nextConfig);
